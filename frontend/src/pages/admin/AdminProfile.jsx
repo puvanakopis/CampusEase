3335c342218b882deccd8e623f4cb66b48d82316
puvanakopis <puvanakopis@gmail.com>
2026-02-11 11:20:38 +0530

    create the admin profile page

--- frontend/src/pages/admin/AdminProfile.jsx
+++ frontend/src/pages/admin/AdminProfile.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AdminSidebar from "../../components/admin/AdminSidebar";
+import AdminOwnerProfilePage from "../../containers/admin/account/AdminOwnerProfilePage";
 
 function AdminProfile() {
     return (
         <div className="bg-[#f6f7f8]">
             <div className="px-4 py-10 md:px-24 max-w-8xl mx-auto gap-6 min-h-screen flex">
                 <AdminSidebar />
+                <AdminOwnerProfilePage />
             </div>
         </div>
     );
