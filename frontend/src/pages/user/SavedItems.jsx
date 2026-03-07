c26a6ac28d2b9e5973a4d8765202fabab36ed1d6
puvanakopis <puvanakopis@gmail.com>
2026-03-08 00:52:56 +0530

    connect saved items page to context to get and remove

--- frontend/src/pages/user/SavedItems.jsx
+++ frontend/src/pages/user/SavedItems.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import SavedItemsPage from "../../containers/user/account/SavedItemsPage";
 import Sidebar from "../../components/user/Sidebar";
+import { SaveItemContext } from "../../context/SaveItemContext";
 
 function SavedItems() {
+    const {
+        savedAccommodations,
+        savedTransports,
+        loading,
+        unsaveAccommodation,
+        unsaveTransport
+    } = useContext(SaveItemContext);
+
     return (
         <div className="bg-[#f6f7f8] ">
             <div className="px-4 py-10 md:px-24 max-w-8xl mx-auto gap-6 min-h-screen flex">
                 <Sidebar />
-                <SavedItemsPage />
+                <SavedItemsPage
+                    accommodations={savedAccommodations}
+                    vehicles={savedTransports}
+                    loading={loading}
+                    onUnsaveAccommodation={unsaveAccommodation}
+                    onUnsaveTransport={unsaveTransport}
+                />
             </div>
         </div>
     );
